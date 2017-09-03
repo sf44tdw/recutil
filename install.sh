578521fc6a4aaa1771c909bbf0a1c26ba78f6d60
sf44tdw <prmkaddr@yahoo.co.jp>
2017-09-03 14:32:05 +0900

    Revert "権限見直し"
    This reverts commit 31bc49747822f29fd0f035afddd4ac1dd588ee66.

--- install.sh
+++ install.sh
@@ -14,10 +14,10 @@ echo '**************************************************************************
 echo "コピー開始。${DESTDIR} -> ${INSTALL_TARGET_DIR}">> ${LOGFILE}
 make_dir_if_not_exist ${INSTALL_TARGET_DIR}
 chmod 711 ./regularly/updateEPG.sh
-cp -f -p ./regularly/updateEPG.sh ${DESTDIR}
+cp -f ./regularly/updateEPG.sh ${DESTDIR}
 cd ${DESTDIR}
 find > ${INSTALL_FILE_LIST} 2>>${LOGFILE}
-cp -r -v -f -p * ${INSTALL_TARGET_DIR} >> ${LOGFILE} 2>&1
+cp -r -v -f * ${INSTALL_TARGET_DIR} >> ${LOGFILE} 2>&1
 echo "コピー完了。${DESTDIR} -> ${INSTALL_TARGET_DIR}">> ${LOGFILE}
 echo '***************************************************************************************************************************************' >> ${LOGFILE}
 
