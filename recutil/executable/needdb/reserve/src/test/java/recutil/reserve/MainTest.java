8bd997cc25d4c499bd1a366625c268770e392f4f
sf44tdw <prmkaddr@yahoo.co.jp>
2017-09-10 05:03:33 +0900

    オプション見直しの続き

--- recutil/executable/needdb/reserve/src/test/java/recutil/reserve/MainTest.java
+++ recutil/executable/needdb/reserve/src/test/java/recutil/reserve/MainTest.java
@@ -10,14 +10,12 @@ import org.junit.After;
 import org.junit.AfterClass;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import recutil.commandexecutor.CommandExecutor;
 import recutil.commandexecutor.DummyExecutor;
-import static recutil.commmonutil.Util.parseLongToString;
 import recutil.consolesnatcher.ConsoleSnatcher;
 import recutil.dbaccessor.testdata.TestData;
 import recutil.loggerconfigurator.LoggerConfigurator;
@@ -78,17 +76,9 @@ public class MainTest {
     private final CommandExecutor exe = new DummyExecutor();
 
     private final String[] args_I = {"-i", TestData.CH3_ID};
-    private final String[] args_I_nf = {"-i", TestData.CH5_ID};
     private final String[] args_V = {"-v", Integer.toString(TestData.PG2_EVENTID)};
-    private final String[] args_S = {"-s", parseLongToString(TestData.PG2_START_TIME)};
 
-    private final String[] args_IV = ArrayUtils.addAll(args_I, args_V);
-    private final String[] args_VS = ArrayUtils.addAll(args_V, args_S);
-    private final String[] args_IS = ArrayUtils.addAll(args_I, args_S);
-
-    private final String[] args_IVS = ArrayUtils.addAll(args_IV, args_S);
-    private final String[] args_IVS_nf = ArrayUtils.addAll(args_VS, args_I_nf);
-
+    
     /**
      * Test of start method, of class Main.
      */
@@ -137,112 +127,7 @@ public class MainTest {
         }
     }
 
-    /**
-     * Test of start method, of class Main.
-     */
-    @Test(expected = org.apache.commons.cli.ParseException.class)
-    public void testStart1_1_4() throws Throwable {
-        try {
-            LOG.info("start1_1_4");
-            String[] args = args_S;
-            Main instance = new Main();
-            instance.start(exe, args);
-        } catch (Throwable ex) {
-            LOG.error("エラー。", ex);
-            throw ex;
-        }
-    }
 
-    /**
-     * Test of start method, of class Main.
-     */
-    @Test(expected = org.apache.commons.cli.ParseException.class)
-    public void testStart1_2_1() throws Throwable {
-        try {
-            LOG.info("start1_2_1");
-            String[] args = args_IV;
-            Main instance = new Main();
-            instance.start(exe, args);
-        } catch (Throwable ex) {
-            LOG.error("エラー。", ex);
-            throw ex;
-        }
-    }
-
-    /**
-     * Test of start method, of class Main.
-     */
-    @Test(expected = org.apache.commons.cli.ParseException.class)
-    public void testStart1_2_2() throws Throwable {
-        try {
-            LOG.info("start1_2_2");
-            String[] args = args_VS;
-            Main instance = new Main();
-            instance.start(exe, args);
-        } catch (Throwable ex) {
-            LOG.error("エラー。", ex);
-            throw ex;
-        }
-    }
-
-    /**
-     * Test of start method, of class Main.
-     */
-    @Test(expected = org.apache.commons.cli.ParseException.class)
-    public void testStart1_3_1() throws Throwable {
-        try {
-            LOG.info("start1_3_1");
-            String[] args = args_IS;
-            Main instance = new Main();
-            instance.start(exe, args);
-        } catch (Throwable ex) {
-            LOG.error("エラー。", ex);
-            throw ex;
-        }
-    }
-
-    /**
-     * Test of start method, of class Main.
-     */
-    @Test
-    public void testStart2_1() throws Throwable {
-        try {
-            LOG.info("start2_1");
-            String[] args = args_IVS;
-            Main instance = new Main();
-            instance.start(exe, args);
-
-            String cmd = exe.getCmd();
-
-            assertEquals(cmd, Main.RESERVE_COMMAND_PARAMS.RESERVE_COMMAND);
-
-            String[] param = exe.getParam();
-            assertEquals(param[0], Main.RESERVE_COMMAND_PARAMS.OPTION_TIME);
-            assertEquals(param[2], Main.RESERVE_COMMAND_PARAMS.OPTION_FILE);
-
-        } catch (Throwable ex) {
-            LOG.error("エラー。", ex);
-            throw ex;
-        }
-    }
-
-    /**
-     * Test of start method, of class Main.
-     */
-    @Test
-    public void testStart2_2() throws Throwable {
-        try {
-            LOG.info("start2_2");
-            String[] args = args_IVS_nf;
-            Main instance = new Main();
-            instance.start(exe, args);
-            String out = this.stdout.getOutput();
-            assertTrue(out.contains("番組が見つかりませんでした"));
-        } catch (Throwable ex) {
-            LOG.error("エラー。", ex);
-            throw ex;
-        }
-    }
 
     /**
      * Test of start method, of class Main.
@@ -251,7 +136,7 @@ public class MainTest {
     public void testStart3_1() throws Throwable {
         try {
             LOG.info("start3_1");
-            String[] args = {"-i", TestData.CH6_ID, "-v", Integer.toString(TestData.PG9_EVENTID), "-s", parseLongToString(TestData.PG9_START_TIME)};
+            String[] args = {"-i", TestData.CH6_ID, "-v", Integer.toString(TestData.PG9_EVENTID)};
             Main instance = new Main();
             instance.start(exe, args);
             String out = this.stdout.getOutput();
